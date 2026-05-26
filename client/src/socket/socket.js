71ad98628a15492df914c2bb8a1a41c603ada884
anuja0802 <anujagupta0802@gmail.com>
2026-05-26 13:26:21 +0000

    fixed

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -17,5 +17,8 @@ const socket = io(SOCKET_URL, {
   reconnectionAttempts: 5,
   reconnectionDelay: 1000, // Wait 1s between reconnect attempts
 });
+socket.on('connect', () => console.log('🟢 Socket connected:', socket.id));
+socket.on('disconnect', (reason) => console.log('🔴 Socket disconnected:', reason));
+socket.on('connect_error', (err) => console.error('Socket error:', err.message));
 
 export default socket;
